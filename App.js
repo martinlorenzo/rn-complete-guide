72e6e6e703637922ed21bb8b61066395a263a54c
Martin Lorenzo <martin.rodrigo.lorenzo@gmail.com>
2021-04-25 18:52:56 -0300

    Game is completed

--- App.js
+++ App.js
@@ -1,14 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { StatusBar} from 'expo-status-bar';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screen/StartGameScreen';
+import GameScreen from './screen/GameScreen';
+import GameOverScreen from './screen/GameOverScreen';
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
+
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(null);
+  }
+
+  const startGameHandler = selectedNumber =>{
+    setUserNumber(selectedNumber);
+  };
+
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds);
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler} />;
+
+  if (userNumber && guessRounds <=0 ){
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>;
+  } else if (guessRounds > 0){
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
+  }
+
   return (
     <View style={styles.screen}>
      <Header title="Guess A Number" />
-      <StartGameScreen />
+      {content}
 
       <StatusBar style="auto" />
     </View>
--- screen/StartGameScreen.js
+++ screen/StartGameScreen.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView, TextInput, Button, ViewPropTypes} from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, StyleSheet,Button, TouchableWithoutFeedback, Keyboard, Alert} from 'react-native';
 import Card from '../components/Card';
 import colors from '../constants/colors';
-import Colors from '../constants/colors';
+import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
+
 
 const StartGameScreen = props => {
-    return (
-        <View style={styles.screen}>
-            <Text style={styles.title}>Start A New Game!</Text>
-            <Card style={styles.inputContainer}>
-                <Text>Select a Number</Text>
-                <TextInput />
-                <View style={styles.buttonContainer}>
-                    <View style={styles.inputButton}>
-                        <Button title="Reset" color={colors.accent} onPress={() =>{}} />
-                    </View> 
-                    <View style={styles.inputButton}>
-                        <Button style={styles.inputButton} color={colors.primary} title="Confirm" onPress={() =>{}} />
-                    </View>
-                </View>
+    const [enteredValue, setEnteredValue] = useState('');
+    const [confirmed, setConfirmedState] = useState(false); 
+    const [selectedNumber, setSelectedNumber] = useState()
+
+    const numberInputHandler = inputText => {
+
+        setEnteredValue(inputText.replace(/[^0-9]/g, ' '));
+    };
+
+
+    const confirmInputHandler = () => {
+        const chosenNumber= parseInt(enteredValue);
+        if (chosenNumber === NaN || chosenNumber <=0 || chosenNumber > 99){
+            Alert.alert(
+                'Invalid Number', 
+                'Number has to be a number between 1 and 99', 
+                [{text: 'Okay', 
+                style:'destructive', 
+                onPress: resetInputHandler}]);
+            return;
+        }
+        setConfirmedState(true);
+        setSelectedNumber(parseInt(enteredValue));
+        setEnteredValue('');
+    }
+
+    const resetInputHandler = () => {
+        setEnteredValue('');
+        setConfirmedState(false);
+    } 
+
+    let confirmedOutput;
+
+    if (confirmed){
+        confirmedOutput = (
+            <Card style={styles.summaryContainer}>
+                <Text>You selected</Text>
+                <NumberContainer>{selectedNumber}</NumberContainer>
+                <Button title="START GAME" onPress={() => props.onStartGame(selectedNumber)} /> 
             </Card>
-        </View>
+        );
+        Keyboard.dismiss();
+    }
+
+    return (
+        <TouchableWithoutFeedback onPress={() => {
+            Keyboard.dismiss();
+        }}>
+            <View style={styles.screen}>
+                <Text style={styles.title}>Start A New Game!</Text>
+                <Card style={styles.inputContainer}>
+                    <Text>Select a Number</Text>
+                    <Input style={styles.input} 
+                        blurOnSubmit 
+                        autoCapitalize='none' 
+                        AutoCorrect={false} 
+                        keyboardType="number-pad"
+                        maxLength= {2}
+                        onChangeText={numberInputHandler}
+                        value={enteredValue}
+                    />
+                    <View style={styles.buttonContainer}>
+                        <View style={styles.inputButton}>
+                            <Button 
+                                title="Reset" 
+                                color={colors.accent} 
+                                onPress={resetInputHandler} 
+                            />
+                        </View> 
+                        <View style={styles.inputButton}>
+                            <Button 
+                                color={colors.primary} 
+                                title="Confirm" 
+                                onPress={confirmInputHandler} 
+                            />
+                        </View>
+                    </View>
+                </Card>
+                {confirmedOutput}
+            </View>
+        </TouchableWithoutFeedback>
     );
 };
 
@@ -58,6 +125,14 @@ const styles = StyleSheet.create({
         width: '40%',
         borderRadius: 40,
         overflow: 'hidden'
+    },
+    input: {
+        width: 30,
+        textAlign: 'center'
+    },
+    summaryContainer: {
+        marginTop: 20,
+        alignItems: 'center'
     }
 });
 
